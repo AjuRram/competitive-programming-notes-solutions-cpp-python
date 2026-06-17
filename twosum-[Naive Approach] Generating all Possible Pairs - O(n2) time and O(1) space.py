66d190be6708aa6a8faabd4c991838aa6a3bbf9f
AjuRram <90497985+AjuRram@users.noreply.github.com>
2026-06-17 08:40:26 +0530

    Add files via upload
    two sum nau=ive approach

---
+++ twosum-[Naive Approach] Generating all Possible Pairs - O(n2) time and O(1) space.py
@@ -0,0 +1,27 @@
+ def twosum(arr,target):
+    n=len(arr)
+
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[i]+arr[j]==target:
+                return True
+                return False
+
+                if_name_=="_main_":
+                arr=[0,-1,2,-3,1]
+                target=-2
+
+                if twosum(arr,target):
+                    print("true")
+                else:
+                    print("false")
+
+                    
+
+
+
+                
+
+
+
+
